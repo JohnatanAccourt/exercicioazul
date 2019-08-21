44bc5607f14eee5d7cbb52d892d7ebbd1e91a11e
johnatan.asantos <johnatan1999accourt@gmail.com>
2019-08-21 16:59:38 -0300

    Initial commit

---
+++ app/src/main/java/com/example/exercicioazul/ActivityPrincipal.java
@@ -0,0 +1,14 @@
+package com.example.exercicioazul;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.principal_layout);
+    }
+}
